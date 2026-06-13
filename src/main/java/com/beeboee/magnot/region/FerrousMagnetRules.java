5b0104414c58498fa4ee0121b0f05f076c3d6882
beeboee <benjamingibsonw@gmail.com>
2026-06-13 04:04:43 -0700

    Sample player body for magnet blocking

--- src/main/java/com/beeboee/magnot/region/FerrousMagnetRules.java
+++ src/main/java/com/beeboee/magnot/region/FerrousMagnetRules.java
@@ -2,6 +2,8 @@ package com.beeboee.magnot.region;
 
 import com.beeboee.magnot.compat.sable.MagnotSableCompat;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import net.neoforged.fml.ModList;
 
@@ -21,5 +23,16 @@ public final class FerrousMagnetRules {
         }
 
         return FerrousRegionSavedData.get(level).blocksMagnet(magnetSource, itemPosition);
+    }
+
+    public static boolean blocksPlayerMagnet(ServerLevel level, Player player, Vec3 itemPosition) {
+        AABB body = player.getBoundingBox();
+        Vec3 feet = player.position();
+        Vec3 bodyCenter = body.getCenter();
+        Vec3 eye = player.getEyePosition();
+
+        return blocksMagnet(level, feet, itemPosition)
+                || blocksMagnet(level, bodyCenter, itemPosition)
+                || blocksMagnet(level, eye, itemPosition);
     }
 }
