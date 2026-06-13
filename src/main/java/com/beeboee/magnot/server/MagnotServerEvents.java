96e5815b2911112b1b488a463e7ca1189f7b8694
beeboee <benjamingibsonw@gmail.com>
2026-06-12 19:46:32 -0700

    Sync and sound ferrous region removals

--- src/main/java/com/beeboee/magnot/server/MagnotServerEvents.java
+++ src/main/java/com/beeboee/magnot/server/MagnotServerEvents.java
@@ -1,15 +1,19 @@
 package com.beeboee.magnot.server;
 
+import com.beeboee.magnot.network.MagnotNetwork;
 import com.beeboee.magnot.region.FerrousRegionSavedData;
 import com.beeboee.magnot.registry.MagnotItems;
+import com.simibubi.create.AllSoundEvents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.phys.Vec3;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 
 public final class MagnotServerEvents {
@@ -37,10 +41,26 @@ public final class MagnotServerEvents {
         Vec3 from = player.getEyePosition();
         Vec3 to = from.add(player.getLookAngle().scale(range));
 
-        boolean removed = FerrousRegionSavedData.get(serverLevel).removeClosestIntersecting(from, to);
-        if (removed) {
+        var removedAt = FerrousRegionSavedData.get(serverLevel).removeClosestIntersecting(from, to);
+        if (removedAt.isPresent()) {
             event.setCanceled(true);
+            AllSoundEvents.SLIME_ADDED.play(serverLevel, null, removedAt.get(), 0.5F, 0.5F);
+            MagnotNetwork.syncToPlayersInDimension(serverLevel);
             player.displayClientMessage(Component.translatable("message.magnot.region_removed"), true);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
+        if (event.getEntity() instanceof ServerPlayer serverPlayer) {
+            MagnotNetwork.syncTo(serverPlayer);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerChangedDimension(PlayerEvent.PlayerChangedDimensionEvent event) {
+        if (event.getEntity() instanceof ServerPlayer serverPlayer) {
+            MagnotNetwork.syncTo(serverPlayer);
         }
     }
 }
