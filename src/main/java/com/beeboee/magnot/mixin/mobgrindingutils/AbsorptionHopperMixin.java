8fe8e504155d538a8a7e472e6d0b58e9f9263c7b
beeboee <benjamingibsonw@gmail.com>
2026-06-14 04:13:11 -0700

    Add Mob Grinding Utils absorption hopper compat

---
+++ src/main/java/com/beeboee/magnot/mixin/mobgrindingutils/AbsorptionHopperMixin.java
@@ -0,0 +1,48 @@
+package com.beeboee.magnot.mixin.mobgrindingutils;
+
+import com.beeboee.magnot.region.FerrousMagnetRules;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+@Pseudo
+@Mixin(targets = "mob_grinding_utils.tile.TileEntityAbsorptionHopper", remap = false)
+public abstract class AbsorptionHopperMixin {
+    @Redirect(
+            method = "getCaptureItems",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/level/Level;getEntitiesOfClass(Ljava/lang/Class;Lnet/minecraft/world/phys/AABB;Ljava/util/function/Predicate;)Ljava/util/List;"
+            ),
+            require = 0
+    )
+    private <T extends Entity> List<T> magnot$filterAbsorptionHopperItems(Level level, Class<T> entityClass, AABB box, Predicate<? super T> predicate) {
+        List<T> candidates = level.getEntitiesOfClass(entityClass, box, predicate);
+        if (!(level instanceof ServerLevel serverLevel) || !ItemEntity.class.isAssignableFrom(entityClass)) {
+            return candidates;
+        }
+
+        BlockEntity blockEntity = (BlockEntity)(Object)this;
+        Vec3 source = magnot$center(blockEntity.getBlockPos());
+        return candidates.stream()
+                .filter(candidate -> !(candidate instanceof ItemEntity item)
+                        || !FerrousMagnetRules.blocksItemPull(serverLevel, source, item))
+                .toList();
+    }
+
+    private static Vec3 magnot$center(BlockPos pos) {
+        return new Vec3(pos.getX() + 0.5D, pos.getY() + 0.5D, pos.getZ() + 0.5D);
+    }
+}
