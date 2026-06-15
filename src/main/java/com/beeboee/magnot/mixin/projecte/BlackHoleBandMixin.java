6acbcc4406071e34ec6812e99236405336ba2489
beeboee <benjamingibsonw@gmail.com>
2026-06-14 22:08:16 -0700

    Make Black Hole Band client visuals filter-aware

--- src/main/java/com/beeboee/magnot/mixin/projecte/BlackHoleBandMixin.java
+++ src/main/java/com/beeboee/magnot/mixin/projecte/BlackHoleBandMixin.java
@@ -3,6 +3,7 @@ package com.beeboee.magnot.mixin.projecte;
 import com.beeboee.magnot.client.ClientFerrousRegionStore;
 import com.beeboee.magnot.compat.sable.MagnotSableClientCompat;
 import com.beeboee.magnot.region.FerrousMagnetRules;
+import com.beeboee.magnot.region.FerrousRegion;
 import moze_intel.projecte.utils.WorldHelper;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
@@ -44,14 +45,35 @@ public abstract class BlackHoleBandMixin {
             return;
         }
 
-        if (ClientFerrousRegionStore.closestIntersecting(target, itemPosition).isPresent()) {
-            return;
-        }
-
-        if (ModList.get().isLoaded("sable") && MagnotSableClientCompat.blocksMagnet(entity.level(), target, itemPosition)) {
+        if (entity instanceof ItemEntity item) {
+            if (magnot$clientBlocksItemPull(item, target)) {
+                return;
+            }
+        } else if (magnot$clientBlocksMagnet(entity, target, itemPosition)) {
             return;
         }
 
         WorldHelper.gravitateEntityTowards(entity, target);
+    }
+
+    private boolean magnot$clientBlocksItemPull(ItemEntity item, Vec3 target) {
+        Vec3 itemTarget = FerrousMagnetRules.itemPullTarget(item);
+
+        boolean worldBlocked = ClientFerrousRegionStore.regions().stream()
+                .filter(FerrousRegion::isWorldRegion)
+                .anyMatch(region -> region.blocksItemPull(target, itemTarget, item.getItem()));
+        if (worldBlocked) {
+            return true;
+        }
+
+        return ModList.get().isLoaded("sable") && MagnotSableClientCompat.blocksItemPull(item.level(), target, item);
+    }
+
+    private boolean magnot$clientBlocksMagnet(Entity entity, Vec3 target, Vec3 itemPosition) {
+        if (ClientFerrousRegionStore.closestIntersecting(target, itemPosition).isPresent()) {
+            return true;
+        }
+
+        return ModList.get().isLoaded("sable") && MagnotSableClientCompat.blocksMagnet(entity.level(), target, itemPosition);
     }
 }
