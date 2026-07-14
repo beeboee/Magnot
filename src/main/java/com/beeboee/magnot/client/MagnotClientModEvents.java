0fe189fa2cef9c8114e975ecdb49fc62e908b405
beeboee <benjamingibsonw@gmail.com>
2026-07-13 18:27:20 -0700

    Register augmenter item decoration

--- src/main/java/com/beeboee/magnot/client/MagnotClientModEvents.java
+++ src/main/java/com/beeboee/magnot/client/MagnotClientModEvents.java
@@ -2,10 +2,12 @@ package com.beeboee.magnot.client;
 
 import com.beeboee.magnot.Magnot;
 import com.beeboee.magnot.registry.MagnotEntityTypes;
+import com.beeboee.magnot.registry.MagnotItems;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+import net.neoforged.neoforge.client.event.RegisterItemDecorationsEvent;
 
 @EventBusSubscriber(modid = Magnot.MOD_ID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
 public final class MagnotClientModEvents {
@@ -15,5 +17,10 @@ public final class MagnotClientModEvents {
     @SubscribeEvent
     public static void registerEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
         event.registerEntityRenderer(MagnotEntityTypes.ferrousRegion(), FerrousRegionEntityRenderer::new);
+    }
+
+    @SubscribeEvent
+    public static void registerItemDecorations(RegisterItemDecorationsEvent event) {
+        event.register(MagnotItems.FIELD_AUGMENTER.get(), FieldAugmenterItemDecorator.INSTANCE);
     }
 }
