217151f3c27208119b21db3652f35d8890b10d9e
beeboee <benjamingibsonw@gmail.com>
2026-07-28 22:37:28 -0700

    Add Forge 1.12.2 compatibility bootstrap

--- ports/forge-1.12.2/src/main/java/com/beeboee/magnot/Magnot.java
+++ ports/forge-1.12.2/src/main/java/com/beeboee/magnot/Magnot.java
@@ -8,12 +8,13 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
         modid = Magnot.MOD_ID,
         name = Magnot.NAME,
         version = Magnot.VERSION,
-        acceptedMinecraftVersions = "[1.12.2]"
+        acceptedMinecraftVersions = "[1.12.2]",
+        dependencies = "required-after:mixinbooter@[10.7,)"
 )
 public final class Magnot {
     public static final String MOD_ID = "magnot";
     public static final String NAME = "Magnot";
-    public static final String VERSION = "1.2.0-alpha.2+mc1.12.2";
+    public static final String VERSION = "1.2.0-alpha.3+mc1.12.2";
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
